2ac40bfc994df43054b30d9e3c91eda05053c3a6
Edubook507 <edubook507@gmail.com>
2024-10-06 23:13:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "15964059"))
 API_HASH = environ.get("API_HASH", "786fd04eca38875885d9427894796c5c")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7130968886:AAGhNfPbD4FA2qgcuESE77Z1DMTIxsY6OMc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]

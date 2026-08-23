0853e52debe8f5c3c50854674d6e6290826262c1
Lucifergod507 <131298140+Lucifergod507@users.noreply.github.com>
2026-08-23 14:58:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -82,8 +82,8 @@ else:
     ON_HEROKU = False
 URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
 
-FSUB_CHANNEL1 = int(environ.get("FSUB_CHANNEL1", "-1002236361577"))
-FSUB_CHANNEL2 = int(environ.get("FSUB_CHANNEL2", "-1001652082524"))
+FSUB_CHANNEL1 = int(environ.get("FSUB_CHANNEL1", "-1001945214205"))
+FSUB_CHANNEL2 = int(environ.get("FSUB_CHANNEL2", "-1002257180578"))
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
